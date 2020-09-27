5276ae82d679a89b4326b7693f5e077a88eded45
mattskeoch <40559749+mattskeoch@users.noreply.github.com>
2020-09-27 17:40:41 +0800

    updated

--- app/js/app.js
+++ app/js/app.js
@@ -9,7 +9,9 @@ const outputIsp = document.querySelector('#outputIsp');
 const button = document.querySelector('#button');
 const error = document.querySelector('#error');
 
+
 const handleClick = () => {
+
     // Selecting the input element and get its value 
     var inputVal = document.getElementById("inputIp").value;
 
@@ -22,14 +24,38 @@ const handleClick = () => {
             if (request.status >= 200 && request.status < 400) {
                 // ip.innerText = data.ip;     
                 outputIp.innerText = data.ip;
-                outputCountry.innerText = data.location.country;
+                // outputCountry.innerText = data.location.country;
                 outputRegion.innerText = data.location.region;
-                outputCity.innerText = data.location.city;
+                outputCity.innerText = data.location.city + ',';
                 outputPostcode.innerText = data.location.postalCode;
                 outputTimezone.innerText = data.location.timezone;
                 outputIsp.innerText = data.isp;
+
+
+                var map = L.map('map').setView([data.location.lat, data.location.lng], 13);
+
+                L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                    maxZoom: 28,
+                    id: 'mapbox/streets-v11',
+                    tileSize: 512,
+                    zoomOffset: -1,
+                    accessToken: 'pk.eyJ1IjoibWF0dHNrIiwiYSI6ImNrZWd3YXRyazBrcnIzMHFxM2NzYnhjazkifQ.tCSKEBGV8JMh7ElHG-arRg'
+                }).addTo(map);
+
+                // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                //     tileSize: 512,
+                //     zoomOffset: -1
+                // }).addTo(map);
+
+                L.marker([data.location.lat, data.location.lng]).addTo(map)
+                    .openPopup();
+
+
+
             } else {
-                outputIsp.innerText = 'ERROR!';
+                error.innerHTML = 'Error: That IP is not valid.' + ' <a href="" onClick="window.location.reload();">Click here</a> ' + 'to refresh the page and try again.';
             }
         }
         request.send()
